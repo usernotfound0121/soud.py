e088a33387dc7da8dd5d42dda50e60517eecf576
usernotfound0121 <134358646+usernotfound0121@users.noreply.github.com>
2023-05-24 10:45:03 +0530

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class example2
+{
+	public static void main(string[ ]args)
+	{
+                    int num1, num2 ,num3, larg , temp;
+                    Scanner sc=new Scanner(System.in);
+                    System.out.print.ln("enter 3 number");
+                   num1=sc.nextInt();
+                   num2=sc.nextInt();
+                   num3=sc.nextInt();
+                   temp=num1>num? num2:num3;
+                   larg=num3>temp? num3:temp;
+                   System.out.print.ln("the largest among of num are,+larg");         
+           
